e7c1505bf1b9344bbefe6997e5b3d3106f6bde3e
Matt Newville <newville@cars.uchicago.edu>
2024-10-28 13:49:16 -0500

    add initial test

---
+++ tests/test_cif2feffinp.py
@@ -0,0 +1,23 @@
+import pytest
+
+from xstructures import get_amcsd, cif2feffinp
+
+def test_cif2feff_v1():
+
+    db = get_amcsd()
+    cifids = {4438: ('S', 'Fe'),
+              4820: ('Ti', 'Fe'),
+              143: ('Fe', 'O')}
+
+    for cifid, atoms in cifids.items():
+        cif = db.get_cif(cifid)
+        for absorber in atoms:
+            for with_h in (True, False):
+                text = cif2feffinp(cif.ciftext,
+                                   absorber=absorber,
+                                   cifid=cifid, cluster_size=6,
+                                   with_h=with_h)
+                assert len(text) > 2000
+
+if __name__ == '__main__':
+    test_cif2feff_v1()
