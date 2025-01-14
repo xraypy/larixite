5e9e5452cb4f01569b9da08caa78862a641de76d
Mauro Rovezzi <mauro.rovezzi@esrf.fr>
2025-01-14 07:35:34 +0100

    start fdmnes

--- larixite/fdmnes.py
+++ larixite/fdmnes.py
@@ -10,6 +10,14 @@ spectroscopy (XAS, XES, RIXS) from the atomic structures
 
 """
 
+from dataclasses import dataclass
+from pathlib import Path
+from .structure import XasStructureGroup
 from .utils import get_logger
 
 logger = get_logger("larixite.fdmnes")
+
+@dataclass
+class FdmnesInput:
+    xsg: XasStructureGroup  #: container for the atomic structure
+    tmplpath: Path  #: path to the FDMNES input template
