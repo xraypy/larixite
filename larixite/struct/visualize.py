5a4e97851c7e170a5e4f9fa85c1733a083b40124
Mauro Rovezzi <mauro.rovezzi@esrf.fr>
2026-05-22 13:15:21 +0200

    use vesta color from pymatgen (hard coded)

--- larixite/struct/visualize.py
+++ larixite/struct/visualize.py
@@ -23,6 +23,119 @@ except ImportError:
     HAS_PY3DMOL = False
 
 
+VESTA_COLORS = {
+    "Ac": "#70abfa",
+    "Ag": "#c0c0c0",
+    "Al": "#81b2d6",
+    "Am": "#545cf2",
+    "Ar": "#cffec4",
+    "As": "#74d057",
+    "At": "#754f45",
+    "Au": "#ffd123",
+    "B": "#1fa20f",
+    "Ba": "#00c900",
+    "Be": "#5ed77b",
+    "Bh": "#e00038",
+    "Bi": "#9e4fb5",
+    "Bk": "#8a4fe3",
+    "Br": "#7e3102",
+    "C": "#4c4c4c",
+    "Ca": "#5a96bd",
+    "Cd": "#ffd98f",
+    "Ce": "#ffffc7",
+    "Cf": "#a136d4",
+    "Cl": "#31fc02",
+    "Cm": "#785ce3",
+    "Co": "#0000af",
+    "Cr": "#00009e",
+    "Cs": "#57178f",
+    "Cu": "#2247dc",
+    "Db": "#d1004f",
+    "Dy": "#1fffc7",
+    "Er": "#00e675",
+    "Es": "#b31fd4",
+    "Eu": "#61ffc7",
+    "F": "#b0b9e6",
+    "Fe": "#b57100",
+    "Fm": "#b31fba",
+    "Fr": "#420066",
+    "Ga": "#9ee373",
+    "Gd": "#45ffc7",
+    "Ge": "#7e6ea6",
+    "H": "#ffcccc",
+    "He": "#fce8ce",
+    "Hf": "#4dc2ff",
+    "Hg": "#b8b8d0",
+    "Ho": "#00ff9c",
+    "Hs": "#e6002e",
+    "I": "#940094",
+    "In": "#a67573",
+    "Ir": "#175487",
+    "K": "#a121f6",
+    "Kr": "#fac1f3",
+    "La": "#5ac449",
+    "Li": "#86df73",
+    "Lr": "#c70066",
+    "Lu": "#00ab24",
+    "Md": "#b30da6",
+    "Mg": "#fb7b15",
+    "Mn": "#a7089d",
+    "Mo": "#54b5b5",
+    "Mt": "#eb0026",
+    "N": "#b0b9e6",
+    "Na": "#f9dc3c",
+    "Nb": "#73c2c9",
+    "Nd": "#c7ffc7",
+    "Ne": "#fe37b5",
+    "Ni": "#b7bbbd",
+    "No": "#bd0d87",
+    "Np": "#0080ff",
+    "O": "#fe0300",
+    "Os": "#266696",
+    "P": "#c09cc2",
+    "Pa": "#00a1ff",
+    "Pb": "#575961",
+    "Pd": "#006985",
+    "Pm": "#a3ffc7",
+    "Po": "#ab5c00",
+    "Pr": "#d9ffc7",
+    "Pt": "#d0d0e0",
+    "Pu": "#006bff",
+    "Ra": "#007d00",
+    "Rb": "#702eb0",
+    "Re": "#267dab",
+    "Rf": "#cc0059",
+    "Rh": "#0a7d8c",
+    "Rn": "#428296",
+    "Ru": "#248f8f",
+    "S": "#fffa00",
+    "Sb": "#9e63b5",
+    "Sc": "#b563ab",
+    "Se": "#9aef0f",
+    "Sg": "#d90045",
+    "Si": "#1b3bfa",
+    "Sm": "#8fffc7",
+    "Sn": "#9a8eb9",
+    "Sr": "#00ff00",
+    "Ta": "#4da6ff",
+    "Tb": "#30ffc7",
+    "Tc": "#3b9e9e",
+    "Te": "#d47a00",
+    "Th": "#00baff",
+    "Ti": "#78caff",
+    "Tl": "#a6544d",
+    "Tm": "#00d452",
+    "U": "#008fff",
+    "V": "#e51900",
+    "W": "#2194d6",
+    "Xe": "#429eb0",
+    "Y": "#94ffff",
+    "Yb": "#00bf38",
+    "Zn": "#8f8f81",
+    "Zr": "#00ff00",
+}
+
+
 def _round_up(x: float) -> float:
     return math.ceil(x * 100) / 100
 
@@ -84,29 +197,9 @@ def visualize(struct: XasStructure, radius: float = 2.5, unitcell: bool = False)
         except AttributeError:
             logger.warning("Unit cell visualization not available for this structure type")
 
-    colors = [
-        "red",
-        "green",
-        "blue",
-        "orange",
-        "yellow",
-        "white",
-        "purple",
-        "pink",
-        "brown",
-        "cyan",
-        "magenta",
-        "olive",
-        "navy",
-        "teal",
-        "maroon",
-        "turquoise",
-        "indigo",
-        "salmon",
-    ]
     color_elems = {}
-    for idx, elem in enumerate(elems):
-        color_elems[elem] = colors[idx]
+    for elem in elems:
+        color_elems[elem] = VESTA_COLORS.get(elem, "#888888")
 
     covalent_r = CovalentRadius().radius
 
@@ -128,6 +221,7 @@ def visualize(struct: XasStructure, radius: float = 2.5, unitcell: bool = False)
 
     absorber_sym = struct.absorber.symbol
     absorber_r = max(0.2, min(covalent_r.get(absorber_sym, 0.5) / 2, 0.75))
+    absorber_color = color_elems.get(absorber_sym, "#888888")
     xyzview.setStyle(
         {"index": 0},
         {
@@ -135,9 +229,9 @@ def visualize(struct: XasStructure, radius: float = 2.5, unitcell: bool = False)
                 "radius": 0.1,
                 "opacity": 1,
                 "hidden": False,
-                "color": "gray",
+                "color": absorber_color,
             },
-            "sphere": {"color": "gray", "radius": absorber_r, "opacity": 1},
+            "sphere": {"color": absorber_color, "radius": absorber_r, "opacity": 0.3},
         },
     )
 
